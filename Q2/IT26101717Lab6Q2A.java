aabdde0e069c7666f85949d3dfc2c8e855240df9
IT26101717 <it26101717@my.sliit.lk>
2026-09-16 09:28:24 +0530

    Add files via upload

---
+++ Q2/IT26101717Lab6Q2A.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IT26101717Lab602A {
+
+    public static void main(String[] args) {
+
+             int counter = 1;
+
+            while (counter <= 10) {
+
+            System.out.print(counter + " ");
+
+            counter++;
+        }
+
+        System.out.println();
+    }
+   }
